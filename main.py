9eb3e3d26a60252d95abf0a63b50437f1cdc5298
Jayrell Quiambao <quiambao.jayrell@gmail.com>
2023-09-14 22:33:44 -0700

    draw function

--- main.py
+++ main.py
@@ -4,15 +4,22 @@ WIDTH, HEIGHT = 600, 400
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("The Game of Nim")
 
+def draw():
+    WINDOW.fill("gray") # change background color
+
+    pygame.display.update() # refresh display to update any drawings
+
 def main():
     run = True
 
     while run:
-        # Check if window is closed
+        # check if window is closed
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 break
+
+        draw()
 
     pygame.quit()
 
