098f2fe9d88a69d452065033d22ddf001627b038
Jayrell Quiambao <quiambao.jayrell@gmail.com>
2023-09-19 01:27:33 -0700

    clean create rows

--- main.py
+++ main.py
@@ -17,38 +17,29 @@ def draw(rowA, rowB, rowC, rowD):
 
     pygame.display.update() # refresh display to update any drawings
 
+def createRow(numSticks):
+    row = []
+    row_X_start = (WIDTH-STICK_WIDTH)/2 - (((numSticks-1)/2) * STICK_WIDTH_SPACE)
+    row_X_end = row_X_start
+    row_Y_start = 75 + (((numSticks-1)/2) * STICK_HEIGHT_SPACE)
+
+    for i in range(numSticks):
+        stick = pygame.Rect(row_X_end, row_Y_start, STICK_WIDTH, STICK_HEIGHT)
+        row.append(stick)
+        row_X_end += STICK_WIDTH_SPACE
+
+    row_X_end = row_X_end - STICK_WIDTH_SPACE + STICK_WIDTH # remove the added spacing at the end and add stick width
+    row_Y_end = row_Y_start + STICK_HEIGHT
+
+    return row, row_X_start, row_X_end, row_Y_start, row_Y_end
+
 def main():
     run = True
 
-    rowA = []
-    rowA_X = (WIDTH - STICK_WIDTH) / 2
-    rowA_Y = 75
-    stick = pygame.Rect(rowA_X, rowA_Y, STICK_WIDTH, STICK_HEIGHT)
-    rowA.append(stick)
-
-    rowB = []
-    rowB_X = rowA_X - STICK_WIDTH_SPACE
-    rowB_Y = rowA_Y + STICK_HEIGHT_SPACE
-    for i in range(3):
-        stick = pygame.Rect(rowB_X, rowB_Y, STICK_WIDTH, STICK_HEIGHT)
-        rowB.append(stick)
-        rowB_X += STICK_WIDTH_SPACE
-    
-    rowC = []
-    rowC_X = rowA_X - 2*STICK_WIDTH_SPACE
-    rowC_Y = rowA_Y + 2*STICK_HEIGHT_SPACE
-    for i in range(5):
-        stick = pygame.Rect(rowC_X, rowC_Y, STICK_WIDTH, STICK_HEIGHT)
-        rowC.append(stick)
-        rowC_X += STICK_WIDTH_SPACE
-    
-    rowD = []
-    rowD_X = rowA_X - 3*STICK_WIDTH_SPACE
-    rowD_Y = rowA_Y + 3*STICK_HEIGHT_SPACE
-    for i in range(7):
-        stick = pygame.Rect(rowD_X, rowD_Y, STICK_WIDTH, STICK_HEIGHT)
-        rowD.append(stick)
-        rowD_X += STICK_WIDTH_SPACE
+    rowA, rowA_X_start, rowA_X_end, rowA_Y_start, rowA_Y_end = createRow(1)
+    rowB, rowB_X_start, rowB_X_end, rowB_Y_start, rowB_Y_end = createRow(3)
+    rowC, rowC_X_start, rowC_X_end, rowC_Y_start, rowC_Y_end = createRow(5)
+    rowD, rowD_X_start, rowD_X_end, rowD_Y_start, rowD_Y_end = createRow(7)
 
     while run:
         for event in pygame.event.get():
@@ -60,14 +51,17 @@ def main():
             if event.type == pygame.MOUSEBUTTONDOWN:
                 pos = pygame.mouse.get_pos()
                 
-                if pos[0] >= rowA_X - 3*STICK_WIDTH_SPACE and pos[0] <= (WIDTH+STICK_WIDTH)/2 + 3*STICK_WIDTH_SPACE:
-                    if pos[1] >= rowA_Y and pos[1] <= rowA_Y + STICK_HEIGHT and len(rowA) >= 1:
+                if pos[0] >= rowA_X_start and pos[0] <= rowA_X_end:
+                    if pos[1] >= rowA_Y_start and pos[1] <= rowA_Y_end and len(rowA) >= 1:
                         rowA.pop(0)
-                    elif pos[1] >= rowB_Y and pos[1] <= rowB_Y + STICK_HEIGHT and len(rowB) >= 1:
+                if pos[0] >= rowB_X_start and pos[0] <= rowB_X_end:
+                    if pos[1] >= rowB_Y_start and pos[1] <= rowB_Y_end and len(rowB) >= 1:
                         rowB.pop(0)
-                    elif pos[1] >= rowC_Y and pos[1] <= rowC_Y + STICK_HEIGHT and len(rowC) >= 1:
+                if pos[0] >= rowC_X_start and pos[0] <= rowC_X_end:
+                    if pos[1] >= rowC_Y_start and pos[1] <= rowC_Y_end and len(rowC) >= 1:
                         rowC.pop(0)
-                    elif pos[1] >= rowD_Y and pos[1] <= rowD_Y + STICK_HEIGHT and len(rowD) >= 1:
+                if pos[0] >= rowD_X_start and pos[0] <= rowD_X_end:
+                    if pos[1] >= rowD_Y_start and pos[1] <= rowD_Y_end and len(rowD) >= 1:
                         rowD.pop(0)
 
         draw(rowA, rowB, rowC, rowD)
