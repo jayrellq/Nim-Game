471f490367dbc0a3c9620439ebdc26e8d64523df
Jayrell Quiambao <quiambao.jayrell@gmail.com>
2023-09-19 19:43:09 -0700

    total sticks + variables

--- main.py
+++ main.py
@@ -27,6 +27,7 @@ def main():
     rowC.create()
     rowD.create()
     rows = [rowA.arr, rowB.arr, rowC.arr, rowD.arr]
+    totalSticks = rowA.numSticks + rowB.numSticks + rowC.numSticks + rowD.numSticks
 
     while run:
         for event in pygame.event.get():
@@ -41,15 +42,19 @@ def main():
                 if pos[0] >= rowA.Xstart and pos[0] <= rowA.Xend:
                     if pos[1] >= rowA.Ystart and pos[1] <= rowA.Yend and len(rowA.arr) >= 1:
                         rowA.arr.pop(0)
+                        totalSticks -= 1
                 if pos[0] >= rowB.Xstart and pos[0] <= rowB.Xend:
                     if pos[1] >= rowB.Ystart and pos[1] <= rowB.Yend and len(rowB.arr) >= 1:
                         rowB.arr.pop(0)
+                        totalSticks -= 1
                 if pos[0] >= rowC.Xstart and pos[0] <= rowC.Xend:
                     if pos[1] >= rowC.Ystart and pos[1] <= rowC.Yend and len(rowC.arr) >= 1:
                         rowC.arr.pop(0)
+                        totalSticks -= 1
                 if pos[0] >= rowD.Xstart and pos[0] <= rowD.Xend:
                     if pos[1] >= rowD.Ystart and pos[1] <= rowD.Yend and len(rowD.arr) >= 1:
                         rowD.arr.pop(0)
+                        totalSticks -= 1
 
         draw(rows)
 
