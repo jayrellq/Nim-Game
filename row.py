a41ef6d0a00abf2bd9a92375566b15d03a137d50
Jayrell Quiambao <quiambao.jayrell@gmail.com>
2023-09-27 21:05:55 -0700

    createRow func edit

--- row.py
+++ row.py
@@ -1,24 +1,24 @@
 import pygame
 from variables import *
 
+# only for rows with odd number of sticks
 class Row():
     def __init__(self, numSticks, Xmid, Ytop):
         self.arr = []
         self.numSticks = numSticks
         self.Xstart = Xmid - (((numSticks-1)/2) * STICK_WIDTH_SPACE)
-        self.Xend = self.Xstart
+        self.Xend = self.Xstart + (numSticks-1)*STICK_WIDTH_SPACE + STICK_WIDTH
         self.Ystart = Ytop + (((numSticks-1)/2) * STICK_HEIGHT_SPACE)
-        self.Yend = self.Ystart
+        self.Yend = self.Ystart + STICK_HEIGHT
 
     # create the sticks for the row
     def createRow(self):
-        for i in range(self.numSticks):
-            stick = pygame.Rect(self.Xend, self.Ystart, STICK_WIDTH, STICK_HEIGHT)
-            self.arr.append(stick)
-            self.Xend += STICK_WIDTH_SPACE
+        tempXstart = self.Xstart
 
-        self.Xend = self.Xend - STICK_WIDTH_SPACE + STICK_WIDTH # remove added spacing at end and add stick width
-        self.Yend = self.Yend + STICK_HEIGHT
+        for i in range(self.numSticks):
+            stick = pygame.Rect(tempXstart, self.Ystart, STICK_WIDTH, STICK_HEIGHT)
+            self.arr.append(stick)
+            tempXstart += STICK_WIDTH_SPACE
 
     # remove a stick from the row
     def removeStick(self):
